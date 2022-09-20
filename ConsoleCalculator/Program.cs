e23f5ffc28f48c94ddcb44e15a2005f78e591890
Samuel Gannon <samgannon@rocketmail.com>
2022-09-20 13:30:25 -0500

    Add project files.

---
+++ ConsoleCalculator/Program.cs
@@ -0,0 +1,11 @@
+﻿// Building a console calculator using microsoft's documentation tutorial
+
+Console.WriteLine("Hello, welcome to your console calculator.");
+Console.WriteLine("Hopefully, following offical documentation won't be to difficult.");
+
+// In this tutorial, I will,
+    // Create a visual studio project
+    // Create a C# console app
+    // debug my app
+    // Inspect my complete code
+
